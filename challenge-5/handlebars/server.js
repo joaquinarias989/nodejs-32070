74109ad66ca566928d7a72d4dfd29a5182282190
Joaquin Arias <joaquinarias989@gmail.com>
2022-08-09 15:50:13 -0300

    Updated the API

--- challenge-5/handlebars/server.js
+++ challenge-5/handlebars/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const Container = require("../Container");
+const products = new Container("../products.txt");
 
 const app = express();
 const routerProds = express.Router();
@@ -9,30 +10,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/products", routerProds);
 
-routerProds.get("/addProduct", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
-});
-
 routerProds.get("/", async (req, res) => {
-  const container = new Container("../products.txt");
-  const products = await container.getAll();
-  products
-    ? res.status(200).json(products)
+  const prods = await products.getAll();
+  prods
+    ? res.status(200).json(prods)
     : res.status(404).json({ error: "No products found" });
 });
 routerProds.get("/:id", async (req, res) => {
   const { id } = req.params;
-  const container = new Container("../products.txt");
-  const product = await container.getById(Number(id));
+  const product = await products.getById(Number(id));
   product
     ? res.status(200).json(product)
     : res.status(404).json({ error: "Product not found" });
 });
 routerProds.post("/", async (req, res) => {
-  const { title, price, stock } = req.body;
-  console.log(title, price, stock);
-  const container = new Container("../products.txt");
-  const idProd = await container.save({ title, price, stock });
+  const { title, price, img } = req.body;
+  console.log(title, price, img);
+  const idProd = await products.save({ title, price, img });
   idProd
     ? res
         .status(201)
@@ -44,9 +38,8 @@ routerProds.post("/", async (req, res) => {
 routerProds.put("/:id", async (req, res) => {
   let { id } = req.params;
   id = Number(id);
-  const { title, price, stock } = req.body;
-  const container = new Container("../products.txt");
-  const product = await container.update({ id, title, price, stock });
+  const { title, price, img } = req.body;
+  const product = await products.update({ id, title, price, img });
   product
     ? res.status(200).json({ message: "Product updated successfully!" })
     : res.status(404).json({
@@ -56,8 +49,7 @@ routerProds.put("/:id", async (req, res) => {
 });
 routerProds.delete("/:id", async (req, res) => {
   let { id } = req.params;
-  const container = new Container("../products.txt");
-  const isDeleted = await container.deleteById(Number(id));
+  const isDeleted = await products.deleteById(Number(id));
   isDeleted
     ? res.status(200).json({ message: "Product deleted successfully!" })
     : res.status(404).json({
