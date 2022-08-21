a31d58ae7500f9c656188697c8f4d707e1fc15d1
Joaquin Arias <joaquinarias989@gmail.com>
2022-08-21 16:35:27 -0300

    Added next parameter to functions

--- challenge-7-pre-entrega/server/controllers/products.controller.js
+++ challenge-7-pre-entrega/server/controllers/products.controller.js
@@ -1,15 +1,18 @@
 const Container = require("../data/Container.js");
 const products = new Container("./data/products.txt");
 
-GetProducts = async (req, res) => {
-  const prods = await products.getAll();
-  prods
-    ? res.status(200).json(prods)
-    : res.status(404).json({ error: "No products found" });
-};
-GetProductById = async (req, res, next) => {
+const GetProducts = async (req, res, next) => {
   try {
-    window.o();
+    const prods = await products.getAll();
+    prods
+      ? res.status(200).json(prods)
+      : res.status(404).json({ error: "No products found" });
+  } catch (error) {
+    next(error);
+  }
+};
+const GetProductById = async (req, res, next) => {
+  try {
     const { id } = req.params;
     const product = await products.getById(Number(id));
     product
@@ -19,39 +22,52 @@ GetProductById = async (req, res, next) => {
     next(error);
   }
 };
-AddProduct = async (req, res) => {
-  const { title, price, img } = req.body;
-  const idProd = await products.save({ title, price, img });
-  idProd
-    ? res
-        .status(201)
-        // .json({ message: "Product added successfully!", idProduct: idProd })
-        .redirect("/AgregarProducto")
-    : res.status(400).json({
-        error: "Something went wrong, the product was not added. Verify error.",
-      });
+const AddProduct = async (req, res, next) => {
+  try {
+    const { title, price, img } = req.body;
+    const idProd = await products.save({ title, price, img });
+    idProd
+      ? res
+          .status(201)
+          // .json({ message: "Product added successfully!", idProduct: idProd })
+          .redirect("/AgregarProducto")
+      : res.status(400).json({
+          error:
+            "Something went wrong, the product was not added. Verify error.",
+        });
+  } catch (error) {
+    next(error);
+  }
 };
-UpdateProduct = async (req, res) => {
-  let { id } = req.params;
-  id = Number(id);
-  const { title, price, img } = req.body;
-  const product = await products.update({ id, title, price, img });
-  product
-    ? res.status(200).json({ message: "Product updated successfully!" })
-    : res.status(404).json({
-        error:
-          "Something went wrong, the product was not updated. Verify error.",
-      });
+const UpdateProduct = async (req, res, next) => {
+  try {
+    let { id } = req.params;
+    id = Number(id);
+    const { title, price, img } = req.body;
+    const product = await products.update({ id, title, price, img });
+    product
+      ? res.status(200).json({ message: "Product updated successfully!" })
+      : res.status(404).json({
+          error:
+            "Something went wrong, the product was not updated. Verify error.",
+        });
+  } catch (error) {
+    next(error);
+  }
 };
-DeleteProduct = async (req, res) => {
-  let { id } = req.params;
-  const isDeleted = await products.deleteById(Number(id));
-  isDeleted
-    ? res.status(200).json({ message: "Product deleted successfully!" })
-    : res.status(404).json({
-        error:
-          "Something went wrong, the product was not deleted. Verify error.",
-      });
+const DeleteProduct = async (req, res, next) => {
+  try {
+    let { id } = req.params;
+    const isDeleted = await products.deleteById(Number(id));
+    isDeleted
+      ? res.status(200).json({ message: "Product deleted successfully!" })
+      : res.status(404).json({
+          error:
+            "Something went wrong, the product was not deleted. Verify error.",
+        });
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
