e23438fd5dbd7c3ba4e5b5adb13d9dfc6dc1033f
Joaquin Arias <joaquinarias989@gmail.com>
2022-08-21 16:05:02 -0300

    Modify the config.js

--- challenge-7-pre-entrega/server/index.js
+++ challenge-7-pre-entrega/server/index.js
@@ -1,7 +1,7 @@
 const { app } = require("./app");
 
 const server = app
-  .listen(PORT, () => {
+  .listen(app.get("port"), () => {
     console.log(`Server running on port ${server.address().port}`);
   })
   .on("error", (err) => {
