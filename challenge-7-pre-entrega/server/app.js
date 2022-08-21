2a4b6f0e2031eb0e79078ffbd1a01440a5d49ac7
Joaquin Arias <joaquinarias989@gmail.com>
2022-08-21 16:00:27 -0300

    Added config.js

--- challenge-7-pre-entrega/server/app.js
+++ challenge-7-pre-entrega/server/app.js
@@ -1,13 +1,20 @@
 const express = require("express");
-const app = express();
+const config = require("./config");
 const routerProds = require("./controllers/products");
 const routerCart = require("./controllers/cart");
 const notFound = require("./middlewares/notFound");
 const handleErrors = require("./middlewares/handleErrors");
 
+const app = express();
+
+//setings
+app.set("port", config.port);
+
+//middlewares
 app.use(express.static(`${__dirname}/public`));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use("/api/products", routerProds);
 app.use("/api/cart", routerCart);
 
