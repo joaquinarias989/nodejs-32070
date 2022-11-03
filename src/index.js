146ac4b985e27a4a7af2e91abcc01236ca92a590
Joaquin Arias <joaquinarias989@gmail.com>
2022-11-03 20:52:51 -0300

    Test for deploy

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const { app } = require("./app");
 
+// console.log("test")
+
 const server = app
   .listen(app.get("port"), () => {
     console.log(`Server running on port ${server.address().port}`);
