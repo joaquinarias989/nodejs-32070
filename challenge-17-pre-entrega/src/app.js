dd63bb960a131de8f91b30f0142eb38cfd6a890d
Joaquin Arias <joaquinarias989@gmail.com>
2022-11-12 10:59:14 -0300

    Added uploads logic

--- challenge-17-pre-entrega/src/app.js
+++ challenge-17-pre-entrega/src/app.js
@@ -3,11 +3,12 @@ const { arg } = require('./config');
 const cors = require('cors');
 const session = require('express-session');
 const passport = require('./middlewares/passport');
+const routerEnv = require('./routes/env.routes');
 const routerAuth = require('./routes/auth.routes');
 const routerProds = require('./routes/products.routes');
 const routerCart = require('./routes/cart.routes');
-const routerEnv = require('./routes/env.routes');
 const routerOrders = require('./routes/orders.routes');
+const routerUploads = require('./routes/uploads.routes');
 const notFound = require('./middlewares/notFound');
 const handleErrors = require('./middlewares/handleErrors');
 const sessionConfig = require('./db/config');
@@ -32,10 +33,11 @@ app.use(session(sessionConfig));
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use('/api/env', routerEnv);
+app.use('/api/uploads', routerUploads);
 app.use('/api/auth', routerAuth);
 app.use('/api/products', routerProds);
 app.use('/api/cart', routerCart);
-app.use('/api/env', routerEnv);
 app.use('/api/orders', routerOrders);
 
 app.use(notFound);
