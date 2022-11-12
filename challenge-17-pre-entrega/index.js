7377401de51a3219210672a124270ade1a4616f7
Joaquin Arias <joaquinarias989@gmail.com>
2022-11-12 11:01:05 -0300

    Separate index file from src

--- challenge-17-pre-entrega/index.js
+++ challenge-17-pre-entrega/index.js
@@ -1,9 +1,9 @@
-const { app } = require("./app");
+const { app } = require('./src/app');
 
 const server = app
-  .listen(app.get("port"), () => {
+  .listen(app.get('port'), () => {
     console.log(`Server running on port ${server.address().port}`);
   })
-  .on("error", (err) => {
+  .on('error', (err) => {
     console.log(err);
   });
