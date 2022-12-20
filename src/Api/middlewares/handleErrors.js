dc2088de88a71007b10542b70a2998bf8323f83d
Joaquin Arias <joaquinarias989@gmail.com>
2022-12-20 11:24:02 -0300

    Fixed notFound error

--- src/Api/middlewares/handleErrors.js
+++ src/Api/middlewares/handleErrors.js
@@ -7,14 +7,14 @@ module.exports = (error, req, res, next) => {
   );
 
   error.success = false;
-  const errorData = error.data.toString();
-  if (errorData.includes('CastError')) {
+  const errorData = error.data?.toString();
+  if (errorData?.includes('CastError')) {
     error.message = 'Debes ingresar un ID válido.';
     res.status(400).json(error);
-  } else if (errorData.includes('duplicate key')) {
+  } else if (errorData?.includes('duplicate key')) {
     error.message = 'El elemento que intentas añadir ya existe';
     res.status(400).json(error);
-  } else if (errorData.includes('format')) {
+  } else if (errorData?.includes('format')) {
     error.message = errorData;
     res.status(400).json(error);
   } else {
